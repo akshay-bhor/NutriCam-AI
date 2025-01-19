077d239566f1ca357cdcbe1bcf9329844366f8b1
akshay-bhor <akbhor50@gmail.com>
2025-01-19 17:03:33 +0530

    init

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"server/routes/authroutess"
+	"server/routes/authroutes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,5 +9,20 @@ import (
 func RegisterAllRouterGroups(server *gin.Engine) {
 	allRoutes := server.Group("/api")
 
-	authroutess.RegisterAuthRoutes(allRoutes)
+	authroutes.RegisterAuthRoutes(allRoutes)
+}
+
+
+package authroutes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterAuthRoutes(server *gin.RouterGroup) {
+	auth := server.Group("/auth")
+
+	auth.POST("/login", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"hi": "hello",
+		})
+	})
 }
