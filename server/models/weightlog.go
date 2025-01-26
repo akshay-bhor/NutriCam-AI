229ec838a8b649ca3153f2f72690a5d641baeb64
akshay-bhor <akbhor50@gmail.com>
2025-01-26 21:06:03 +0530

    Remove server from git tracking

--- server/models/weightlog.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type WeightLog struct {
-	UserId     uint    `gorm:"unique;not null:true" json:"user_id"`
-	Weight     float64 `gorm:"not null:true;type:float(3, 1)" json:"weight"`
-	Weight_lbs float64 `gorm:"not null:true;type:float(3, 1)" json:"weight_lbs"`
-}
